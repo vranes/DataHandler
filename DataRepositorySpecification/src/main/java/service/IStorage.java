c1b3a19d3df55b077b3f42c31a0dd8d4bdaa6c39
vranes <nikolinavranes@gmail.com>
2020-10-26 21:56:03 +0100

    specification Database edit

--- DataRepositorySpecification/src/main/java/service/IStorage.java
+++ DataRepositorySpecification/src/main/java/service/IStorage.java
@@ -1,7 +1,6 @@
 package service;
 
 import model.Entity;
-
 import java.util.List;
 import java.util.Map;
 
@@ -15,11 +14,9 @@ public interface IStorage {
     public void add (String path, String id, String name, Map<String, String> attributes, Map<String, Entity> nestedEntities) throws Exception;
 
     // probaj da koristis ove metode a ne ove ispod, pretrazujemo listu entiteta iz Database-a
-    public Entity findByIdLocal(String id);
-    public List<Entity> findByTypeLocal(String type);
+    public Entity findById(String id);
+    public List<Entity> findByType(String type);
+    public List<Entity> findByAttribute(String name);
+    public List<Entity> findByValue(String operator, String name, String value);
 
-    /*
-    public <T> T findById(String path, String id);
-    public <T> List<T> findByType(String path, String type);
-    */
 }
